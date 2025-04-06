d89a83cd010ce41de4306d127266fe617a9fbb84
sakthiarajapandian <sakthiapandian27@gmail.com>
2025-04-06 10:19:30 +1000

    task6.1p push

---
+++ task6.1p/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 80;
+app.get('/', (req, res) => {
+    res.send('Hello, Docker!');
+});
+
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
